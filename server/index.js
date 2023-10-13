a6a9b80f19f637e001a2ffbc54ffa9447a62e706
Diego Gutiérrez <diego804@gmail.com>
2023-10-13 13:37:19 -0500

    If a messages exist then show messages

--- server/index.js
+++ server/index.js
@@ -29,7 +29,7 @@ await db.execute(`
   )
 `)
 
-io.on("connection", (socket)=>{
+io.on("connection", async (socket)=>{
   console.log('a user connected')
 
   socket.on("disconnect", () => {
@@ -50,6 +50,20 @@ io.on("connection", (socket)=>{
 
     io.emit('chat message', msg, result.lastInsertRowid.toString())
   })
+
+  if(!socket.recovered) {
+    try {
+      const results = await db.execute({
+        sql: `SELECT id, content FROM messages WHERE id > ?`,
+        args: [socket.handshake.auth.serverOffset ?? 0]
+      })
+      results.rows.forEach(row => {
+        socket.emit("chat message", row.content, row.id.toString())
+      })
+    } catch (e){
+      console.error(e)
+    }
+  }
 })
 
 app.use(logger("dev"))
